c78e68616d2f93668d8803236ff610e63bc9844a
David Chu <45278895+thedavidchu@users.noreply.github.com>
2021-01-30 21:53:03 -0500

    Update gradient_descent_plotter.py

--- gradient_descent_plotter.py
+++ gradient_descent_plotter.py
@@ -46,34 +46,39 @@ def plot(trn_loss=None, trn_acc=None, trn_batch_per_epoch=1,
     :return: None
     """
 
+    # Declare constants
+    TRN_CLR = 'g-'
+    VAL_CLR = 'r-'
+    TST_CLR = 'k-'
+
+    # Space out training x-axis
+    if trn_loss is not None:
+        trn_x = np.linspace(0, (len(trn_loss) - 1) / trn_batch_per_epoch, len(trn_loss))
+    elif trn_acc is not None:
+        trn_x = np.linspace(0, (len(trn_acc) - 1) / trn_batch_per_epoch, len(trn_acc))
+    # Space out validation x-axis
+    if val_loss is not None:
+        val_x = np.linspace(0, (len(val_loss) - 1) / val_batch_per_epoch, len(val_loss))
+    elif val_acc is not None:
+        val_x = np.linspace(0, (len(val_acc) - 1) / val_batch_per_epoch, len(val_acc))
+    # Space out testing x-axis
+    if tst_loss is not None:
+        tst_x = np.linspace(0, (len(tst_loss) - 1) / tst_batch_per_epoch, len(tst_loss))
+    elif tst_acc is not None:
+        tst_x = np.linspace(0, (len(tst_acc) - 1) / tst_batch_per_epoch, len(tst_acc))
+
     # Plot Expanded Nodes
     fig, axs = plt.subplots(1, 2)
     fig.canvas.set_window_title(figure)
     fig.suptitle(title)
 
-    # Space out training x-axis
-    if trn_loss is not None:
-        trn_x = np.linspace(0, (len(trn_loss) - 1)/trn_batch_per_epoch, len(trn_loss))
-    elif trn_acc is not None:
-        trn_x = np.linspace(0, (len(trn_acc) - 1) / trn_batch_per_epoch, len(trn_acc))
-    # Space out validation x-axis
-    if val_loss is not None:
-        val_x = np.linspace(0, (len(val_loss) - 1)/val_batch_per_epoch, len(val_loss))
-    elif val_acc is not None:
-        val_x = np.linspace(0, (len(val_acc) - 1) / val_batch_per_epoch, len(val_acc))
-    # Space out testing x-axis
-    if tst_loss is not None:
-        tst_x = np.linspace(0, (len(tst_loss) - 1)/tst_batch_per_epoch, len(tst_loss))
-    elif tst_acc is not None:
-        tst_x = np.linspace(0, (len(tst_acc) - 1) / tst_batch_per_epoch, len(tst_acc))
-
     # Plot loss
     if trn_loss is not None:
-        axs[0].plot(trn_x, trn_loss, 'g', label='Training')
+        axs[0].plot(trn_x, trn_loss, TRN_CLR, label='Training')
     if val_loss is not None:    # Plot validation loss if applicable
-        axs[0].plot(val_x, val_loss, 'r', label='Validation')
+        axs[0].plot(val_x, val_loss, VAL_CLR, label='Validation')
     if tst_loss is not None:    # Plot test loss if applicable
-        axs[0].plot(tst_x, tst_loss, 'k', label='Test')
+        axs[0].plot(tst_x, tst_loss, TST_CLR, label='Test')
     axs[0].set_title(f'Loss vs Epoch')
     axs[0].set_xlabel('Epoch')
     axs[0].set_ylabel('Loss')
@@ -81,11 +86,11 @@ def plot(trn_loss=None, trn_acc=None, trn_batch_per_epoch=1,
 
     # Plot accuracy
     if trn_acc is not None:
-        axs[1].plot(trn_x, trn_acc, 'g', label='Training')
+        axs[1].plot(trn_x, trn_acc, TRN_CLR, label='Training')
     if val_acc is not None:     # Plot validation accuracy if applicable
-        axs[1].plot(val_x, val_acc, 'r', label='Validation')
+        axs[1].plot(val_x, val_acc, VAL_CLR, label='Validation')
     if tst_acc is not None:     # Plot test accuracy if applicable
-        axs[1].plot(tst_x, tst_acc, 'k', label='Test')
+        axs[1].plot(tst_x, tst_acc, TST_CLR, label='Test')
     axs[1].set_title(f'Accuracy vs Epoch')
     axs[1].set_xlabel('Epoch')
     axs[1].set_ylabel('Accuracy')
